e0f79f3682b3caf0795ab1d611aecac5585dfb60
jmorgan <jamorgan8@gmail.com>
2022-08-06 13:17:22 -0500

    Done, continue to double check

--- script/scripts.sql
+++ script/scripts.sql
@@ -53,20 +53,23 @@ WHERE location = 'CA';
 /*Q9: Find the name of each company and its average star 
 rating for all companies that have more than 5000 reviews 
 across all locations. How many companies are there with more that 5000 reviews across all locations? */
-SELECT company, AVG(star_rating) AS avg_star
+SELECT company, AVG(star_rating) AS avg_star, review_count
 FROM data_analyst_jobs
 WHERE review_count > '5000'
-GROUP BY company;
---Answer-- 41 companies where reviews>5k
+      AND company IS NOT NULL
+GROUP BY company, review_count
+ORDER BY review_count DESC;
+--Answer-- 45 companies where reviews>5k
 
 /*Q10: Add the code to order the query in #9 from highest to lowest average star rating. 
 Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating? */
-SELECT company, AVG(star_rating) AS avg_star
+SELECT company, AVG(star_rating) AS avg_star, review_count
 FROM data_analyst_jobs
 WHERE review_count > '5000'
-GROUP BY company
+      AND company IS NOT NULL
+GROUP BY company, review_count
 ORDER BY avg_star DESC;
---Answer-- 3 way tie between GM, Unilever, and Microsoft
+--Answer-- 3 way tie between American Express, Kaiser Permanente, and GM
 
 --Q11: Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
 SELECT DISTINCT(title)
